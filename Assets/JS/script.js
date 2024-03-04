3351c68304acca415f3361b5df968503079198a7
Marion Cayabyab <155805133+RionCayabyab@users.noreply.github.com>
2024-03-04 15:44:21 +0800

    Add files via upload

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,32 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const playlist = document.getElementById("playlist");
-  const popup = document.getElementById("popup");
-  const card = document.getElementById("card");
-  const popupAlbumCover = document.getElementById("popupAlbumCover");
-  const popupSongTitle = document.getElementById("popupSongTitle");
-  const audio = document.getElementById("audio");
-  const closeBtn = document.getElementById("closeBtn");
+function changeColor() {
+  var container = document.querySelector(".container");
 
-  popup.style.display = "none";
+  var currentColor = container.style.backgroundColor;
 
-  playlist.addEventListener("click", function (e) {
-    if (e.target.tagName === "LI") {
-      const songSrc = e.target.getAttribute("data-src");
-      const albumImg = e.target.getAttribute("data-img");
-      const songTitle = e.target.textContent;
-
-      popupAlbumCover.src = albumImg;
-      popupSongTitle.textContent = songTitle;
-      audio.src = songSrc;
-
-      popup.style.display = "block";
-
-      audio.autoplay = true;
-    }
-  });
-
-  closeBtn.addEventListener("click", function () {
-    popup.style.display = "none";
-    audio.pause();
-  });
-});
+  if (currentColor === "rgb(229, 251, 229)") {
+    container.style.backgroundColor = "white";
+    container.style.color = "#8abd91";
+  } else {
+    container.style.backgroundColor = "#e5fbe5";
+    container.style.color = "#154734";
+  }
+}
